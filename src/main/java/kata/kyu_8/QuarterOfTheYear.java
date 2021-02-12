308aa7716c97d78f38690fb5541be6617be73019
Yulian Belov <belovyulian@gmail.com>
2021-02-12 20:49:54 +0200

    commit

---
+++ src/main/java/kata/kyu_8/QuarterOfTheYear.java
@@ -0,0 +1,13 @@
+package kata.kyu_8;
+
+public class QuarterOfTheYear {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static int getQuarter(int month) {
+
+    return (int) Math.ceil(month / 3.);
+  }
+}
