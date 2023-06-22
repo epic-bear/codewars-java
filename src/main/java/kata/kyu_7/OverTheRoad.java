eaf65db4df276676d674443d4ef0b6f020312845
Yulian Belov <belovyulian@gmail.com>
2023-06-22 14:15:10 +0300

    OverTheRoad

---
+++ src/main/java/kata/kyu_7/OverTheRoad.java
@@ -0,0 +1,12 @@
+package kata.kyu_7;
+
+public class OverTheRoad {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static long overTheRoad(long address, long n) {
+    return n * 2 + 1 - address;
+  }
+}
