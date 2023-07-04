5847744d8922969bd15effc67eca31c5e9d71b36
Yulian Belov <belovyulian@gmail.com>
2023-07-04 11:36:44 +0300

    BetweenExtremes

---
+++ src/main/java/kata/kyu_7/BetweenExtremes.java
@@ -0,0 +1,15 @@
+package kata.kyu_7;
+
+import java.util.Arrays;
+
+public class BetweenExtremes {
+
+  public static void main(String[] args) {
+    System.out.println(betweenExtremes(new int[]{21, 34, 54, 43, 26, 12}));
+  }
+
+  public static int betweenExtremes(int[] numbers) {
+    Arrays.sort(numbers);
+    return numbers[numbers.length - 1] - numbers[0];
+  }
+}
