0f40f9d4f13a8df9623425062f4ff7b675de9b70
Yulian Belov <belovyulian@gmail.com>
2023-08-05 18:25:36 +0300

    ThinkingAndTesting

---
+++ src/main/java/kata/kyu_7/ThinkingAndTesting.java
@@ -0,0 +1,12 @@
+package kata.kyu_7;
+
+public class ThinkingAndTesting {
+
+  public static void main(String[] args) {
+    System.out.println(testAB(0, 1));
+  }
+
+  public static int testAB(int a, int b) {
+    return a | b;
+  }
+}
