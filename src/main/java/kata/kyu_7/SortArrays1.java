b252ee1c747078e8d850345cab4426196757bbb9
Yulian Belov <belovyulian@gmail.com>
2023-09-14 12:31:26 +0300

    SortArrays1

---
+++ src/main/java/kata/kyu_7/SortArrays1.java
@@ -0,0 +1,35 @@
+package kata.kyu_7;
+
+import java.util.Arrays;
+
+public class SortArrays1 {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static <T> T[] sortArray(T[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+
+  public static int[] sortArray(int[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+
+  public static long[] sortArray(long[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+
+  public static double[] sortArray(double[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+
+  public static float[] sortArray(float[] names) {
+    Arrays.sort(names);
+    return names;
+  }
+}
