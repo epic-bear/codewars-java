68cd8a3aba9374e1b3d88a3ab745538c7f2ebffd
Yulian Belov <ybelo@softserveinc.com>
2023-09-24 14:52:54 +0300

    test

--- src/main/java/kata/kyu_7/BetweenExtremes.java
+++ src/main/java/kata/kyu_7/BetweenExtremes.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class BetweenExtremes {
 
-  public static void main(String[] args) {
-    System.out.println(betweenExtremes(new int[]{21, 34, 54, 43, 26, 12}));
-  }
+    public static void main(String[] args) {
+        System.out.println(betweenExtremes(new int[]{21, 34, 54, 43, 26, 12}));
+    }
 
-  public static int betweenExtremes(int[] numbers) {
-    Arrays.sort(numbers);
-    return numbers[numbers.length - 1] - numbers[0];
-  }
+    public static int betweenExtremes(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1] - numbers[0];
+    }
 }
