e6f2929cac981d0a34f9c29b0ddbb8f6df954ea7
Yulian Belov <ybelo@softserveinc.com>
2023-09-24 14:58:56 +0300

    test

--- src/main/java/kata/kyu_7/BinaryAddition.java
+++ src/main/java/kata/kyu_7/BinaryAddition.java
@@ -2,11 +2,11 @@ package kata.kyu_7;
 
 public class BinaryAddition {
 
-  public static void main(String[] args) {
-    System.out.println(binaryAddition(1, 1));
-  }
+    public static void main(String[] args) {
+        System.out.println(binaryAddition(1, 1));
+    }
 
-  public static String binaryAddition(int a, int b){
-    return Long.toBinaryString(a + b);
-  }
+    public static String binaryAddition(int a, int b) {
+        return Long.toBinaryString(a + b);
+    }
 }
