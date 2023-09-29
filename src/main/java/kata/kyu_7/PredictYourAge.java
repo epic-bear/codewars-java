9158c7c770a00662c4bb199acbb80ad70dc16266
Yulian Belov <ybelo@softserveinc.com>
2023-09-29 16:57:07 +0300

    PredictYourAge

--- src/main/java/kata/kyu_7/PredictYourAge.java
+++ src/main/java/kata/kyu_7/PredictYourAge.java
@@ -8,8 +8,8 @@ public class PredictYourAge {
         System.out.println(predictAge(65, 60, 75, 55, 60, 63, 64, 45));
     }
 
-    public static int predictAge(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8) {
+    public static int predictAge(int ... ages) {
         return (int) Math.floor(Math.sqrt(
-                Arrays.stream(new int[]{age1, age2, age3, age4, age5, age6, age7, age8}).map(x -> x * x).sum()) / 2);
+                Arrays.stream(ages).map(x -> x * x).sum()) / 2);
     }
 }
