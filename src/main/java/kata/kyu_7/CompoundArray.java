a8ed19035107eff28a730544a47aa35881a9b2b4
Yulian Belov <ybelo@softserveinc.com>
2023-12-27 13:02:14 +0200

    commit

--- src/main/java/kata/kyu_7/CompoundArray.java
+++ src/main/java/kata/kyu_7/CompoundArray.java
@@ -5,22 +5,14 @@ import java.util.Arrays;
 public class CompoundArray {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(compoundArray(new int[]{1, 2, 3, 4, 5, 6}, new int[]{9, 8, 7, 6})));
+        System.out.println(Arrays.toString(compoundArray(new int[]{0, 1, 2}, new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0})));
     }
 
     public static int[] compoundArray(int[] a, int[] b) {
         int[] result = new int[a.length + b.length];
-        int[] min = Math.min(a.length, b.length) == a.length ? a : b;
-        int[] max = Math.max(a.length, b.length) == a.length ? a : b;
-
-        for (int j = 0, i = 0; j < max.length; i += 2, j++) {
-            if (j < min.length) {
-                result[i] = a[j];
-                result[i + 1] = b[j];
-            } else {
-                result[i] = max[j];
-                result[i + 1] = max[++j];
-            }
+        for (int i = 0, j = 0; i < result.length; j++) {
+            if (j < a.length) result[i++] = a[j];
+            if (j < b.length) result[i++] = b[j];
         }
         return result;
     }
