d6a6be3388798742734a5fdf6febb9df655569c1
Yulian Belov <ybelo@softserveinc.com>
2024-02-25 10:45:51 +0200

    commit

---
+++ src/main/java/kata/kyu_8/GrasshopperCombineStrings.java
@@ -0,0 +1,12 @@
+package kata.kyu_8;
+
+public class GrasshopperCombineStrings {
+
+    public static void main(String[] args) {
+        System.out.println(combineNames("James", "Stevens"));
+    }
+
+    public static String combineNames(String first, String last) {
+        return first + " " + last;
+    }
+}
