b21a6fa100c6ca0ca97bdac4d837af789b967d22
ybelo <belovyulian@gmail.com>
2025-09-28 15:32:59 +0300

    update

--- src/main/java/kata/kyu_7/TheHuntingOfTheSnark.java
+++ src/main/java/kata/kyu_7/TheHuntingOfTheSnark.java
@@ -2,18 +2,32 @@ package kata.kyu_7;
 
 public class TheHuntingOfTheSnark {
 
-  public static void main(String[] args) {
-    int[] booj =
-        {0, 0, 2, 1, 0, 0, 4, 2, 0, 0, 16, 12, 8, 5, 0, 0, 2};
-    String[] bloog =
-        {"giant horse", "the hog", "", "snork", "", "", "deadly snake", "chipmunk", "a dingus",
-            "The", "snark", "allgrimble", "sneet", "sningus", "snart", "", "slungus"};
-    System.out.println(assess(booj, bloog));
-  }
+    public static void main(String[] args) {
+        int[] booj =
+                {0, 0, 36, 1, 0, 0, 4, 2, 2, 437, 2, 0, 8, 0, 0, 2, 1};
+        String[] bloog =
+                {"blimpy", "", "", "", "", "", "also a horse but not giant", "", "a doofus", "literally Zeus", "", "", "", "", "snart","","snark"};
 
-  public static String assess(int[] booj, String[] bloog) {
-    //What do we do, captain?
+        System.out.println(assess(booj, bloog));
+    }
 
-    return "";
-  }
+    public static String assess(int[] booj, String[] bloog) {
+        for (int i = 0; i <= 8; i++) {
+            int left = 8 - i;
+            int right = 8 + i;
+
+            if (bloog[left].equals("snark") || bloog[right].equals("snark")) {
+                int boojumeter = bloog[left].equals("snark") ? booj[left] : booj[right];
+
+                if (i <= 2) {
+                    return boojumeter >= 15 ? "Run!" : "Pounce!";
+                } else if (i <= 4) {
+                    return boojumeter >= 15 ? "Escape quietly..." : "Stalk quietly...";
+                } else {
+                    return boojumeter >= 15 ? "There's a Boojum close." : "There's a Snark close.";
+                }
+            }
+        }
+        return "Keep hunting.";
+    }
 }
